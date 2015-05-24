cbf33710c455f422b511a831a1cf92c2061ead98
Kudzai Nyandoro <kudzain@gmail.com>
2015-05-24 10:22:01 -0700

    Add Admin/Post.

--- app/Http/Controllers/Admin/PostController.php
+++ app/Http/Controllers/Admin/PostController.php
@@ -1,84 +1,100 @@
 <?php namespace App\Http\Controllers\Admin;
 
+use App\Commands\PostFormFields;
 use App\Http\Requests;
+use App\Http\Requests\PostCreateRequest;
+use App\Http\Requests\PostUpdateRequest;
 use App\Http\Controllers\Controller;
+use App\Post;
 
-use Illuminate\Http\Request;
+class PostController extends Controller
+{
 
-class PostController extends Controller {
+  /**
+   * Display a listing of the posts.
+   */
+  public function index()
+  {
+    return view('admin.post.index')
+      ->withPosts(Post::all());
+  }
 
-	/**
-	 * Display a listing of the resource.
-	 *
-	 * @return Response
-	 */
-	public function index()
-	{
-		   return view('admin.post.index');
-	}
+  /**
+   * Show the new post form
+   */
+  public function create()
+  {
+    $data = $this->dispatch(new PostFormFields());
 
-	/**
-	 * Show the form for creating a new resource.
-	 *
-	 * @return Response
-	 */
-	public function create()
-	{
-		//
-	}
+    return view('admin.post.create', $data);
+  }
 
-	/**
-	 * Store a newly created resource in storage.
-	 *
-	 * @return Response
-	 */
-	public function store()
-	{
-		//
-	}
+  /**
+   * Store a newly created Post
+   *
+   * @param PostCreateRequest $request
+   */
+  public function store(PostCreateRequest $request)
+  {
+    $post = Post::create($request->postFillData());
+    $post->syncTags($request->get('tags', []));
 
-	/**
-	 * Display the specified resource.
-	 *
-	 * @param  int  $id
-	 * @return Response
-	 */
-	public function show($id)
-	{
-		//
-	}
+    return redirect()
+        ->route('admin.post.index')
+        ->withSuccess('New Post Successfully Created.');
+  }
 
-	/**
-	 * Show the form for editing the specified resource.
-	 *
-	 * @param  int  $id
-	 * @return Response
-	 */
-	public function edit($id)
-	{
-		//
-	}
+  /**
+   * Show the post edit form
+   *
+   * @param  int  $id
+   * @return Response
+   */
+  public function edit($id)
+  {
+    $data = $this->dispatch(new PostFormFields($id));
 
-	/**
-	 * Update the specified resource in storage.
-	 *
-	 * @param  int  $id
-	 * @return Response
-	 */
-	public function update($id)
-	{
-		//
-	}
+    return view('admin.post.edit', $data);
+  }
 
-	/**
-	 * Remove the specified resource from storage.
-	 *
-	 * @param  int  $id
-	 * @return Response
-	 */
-	public function destroy($id)
-	{
-		//
-	}
+  /**
+   * Update the Post
+   *
+   * @param PostUpdateRequest $request
+   * @param int  $id
+   */
+  public function update(PostUpdateRequest $request, $id)
+  {
+    $post = Post::findOrFail($id);
+    $post->fill($request->postFillData());
+    $post->save();
+    $post->syncTags($request->get('tags', []));
 
+    if ($request->action === 'continue') {
+      return redirect()
+          ->back()
+          ->withSuccess('Post saved.');
+    }
+
+    return redirect()
+        ->route('admin.post.index')
+        ->withSuccess('Post saved.');
+  }
+
+  /**
+   * Remove the specified resource from storage.
+   *
+   * @param  int  $id
+   * @return Response
+   */
+  public function destroy($id)
+  {
+    $post = Post::findOrFail($id);
+    $post->tags()->detach();
+    $post->delete();
+
+    return redirect()
+        ->route('admin.post.index')
+        ->withSuccess('Post deleted.');
+  }
 }
