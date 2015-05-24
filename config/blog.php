4052068d8a2971c6c80c18a6c398cb22478fb696
Kudzai Nyandoro <kudzain@gmail.com>
2015-05-24 06:52:02 -0700

    Add Amazon s3.

--- config/blog.php
+++ config/blog.php
@@ -1,5 +1,9 @@
 <?php
 return [
-    'title' => 'My Blog',
-    'posts_per_page' => 5
+  'title' => 'My Blog',
+  'posts_per_page' => 5,
+  'uploads' => [
+    'storage' => 's3',
+    'webpath' => 'https://s3-us-west-1.amazonaws.com/laravel-kudzai/',
+  ],
 ];
